8935c963a35328b3a842f8d8e3c7a58b93bbd22e
richarddroid557 <richrich557@yahoo.com>
2024-08-04 16:42:33 -0600

    bcfvfn

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ document.getElementById('get-weather').addEventListener('click', function() {
             if (data) {
                 const weatherInfo = `
                     <p><strong>${data.location.name}, ${data.location.country}</strong></p>
-                    <p>Temperature: ${data.current.temp_f}°C</p>
+                    <p>Temperature: ${data.current.temp_f}°F</p>
                     <p>Weather: ${data.current.condition.text}</p>
                     <p>UV: ${data.current.uv}</p>
                 `;
